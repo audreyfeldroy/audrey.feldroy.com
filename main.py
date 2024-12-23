246b9fa77924876d5a7a1f03b1c594332c1ee0c3
Audrey M. Roy Greenfeld <arg@answer.ai>
2024-12-23 12:18:23 +0000

    Add single nb handler

--- main.py
+++ main.py
@@ -1,19 +1,26 @@
 from pathlib import Path
 from fasthtml.common import *
-from nb2fasthtml.core import (
-    render_nb, read_nb, get_frontmatter_raw,render_md,
-    strip_list
-)
+from nb2fasthtml.core import *
 
 app,rt = fast_app()
 
 @rt
 def index():
     nbs = L(Path('nbs').glob('*.ipynb'))
+    nbs = sorted(nbs)
     return Div(
         H1('My nb2fasthtml-powered blog'), 
-        Ul(*[Li(A(nb.stem, href=f'/nb/{nb.stem}')) for nb in nbs]),
+        Ul(*[Li(A(nb.stem, href=f'/experiments/{nb.stem}')) for nb in nbs]),
         style="padding: 1em"
     )
+
+@rt('/experiments/{name}')
+def experiment(name: str):
+    nb = Path(f'nbs/{name}.ipynb')
+    return Div(
+        render_nb(nb),
+        style="padding: 1em"
+    )
+
 
 serve()
